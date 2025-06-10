253f83f049bc6b9b4a7d329f538fba1d4ac5507a
Emile Avoscan <85992291+AvoscanEmile@users.noreply.github.com>
2025-06-10 16:39:33 -0600

    Update installation.sh
    Fixed a couple of syntax problems

--- install/installation.sh
+++ install/installation.sh
@@ -67,7 +67,8 @@ EOF
 # 7. Minimal single-user Nix installation for AlmaLinux 9.6
 # a. Create /nix store directory with current user permissions and call user-nix-setup at user-level
 install -d -m755 -o "$SUDO_UID" -g "$SUDO_GID" /nix
-sudo -u "$SUDO_USER" bash /home/"$SUDO_USER"/user-nix-setup.sh
+USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+sudo -u "$SUDO_USER" bash "$USER_HOME/SoulmateOS/install/user-nix-setup.sh"
 
 # 8. Reboot prompt 
 printf "\n Installation complete.\n"
