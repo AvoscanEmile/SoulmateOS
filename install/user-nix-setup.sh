e0577362c1b3c437d2cb7762571ee2e0956b8a7e
Emile Avoscan <85992291+AvoscanEmile@users.noreply.github.com>
2025-06-10 16:22:26 -0600

    Create user-nix-setup.sh

---
+++ install/user-nix-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# 1. Run the official Nix installer in no-daemon mode
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+
+# 2. Source Nix into the current shell and persist it
+. "$HOME/.nix-profile/etc/profile.d/nix.sh"
+source "$HOME/.nix-profile/etc/profile.d/nix.sh" >> ~/.bashrc
