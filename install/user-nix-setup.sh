cedc239dda712c2dba4b312f7412b2f108e007ff
Emile Avoscan <85992291+AvoscanEmile@users.noreply.github.com>
2025-06-10 17:17:07 -0600

    Update user-nix-setup.sh
    Added exe bash to the end to avoid a reboot.

--- install/user-nix-setup.sh
+++ install/user-nix-setup.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # 1. Run the official Nix installer in no-daemon mode
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
 
-# 2. Source Nix into the current shell and persist it
+# 2. Source Nix into the current shell and persist it, after that reloads bash to avoid reboot
 . "$HOME/.nix-profile/etc/profile.d/nix.sh"
 echo 'source "$HOME/.nix-profile/etc/profile.d/nix.sh"' >> "$HOME/.bashrc"
-
+exec bash
