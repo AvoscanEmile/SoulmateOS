fd374670b3c0f15bc441a7a8ec41f430cb7dca13
Emile Avoscan <85992291+AvoscanEmile@users.noreply.github.com>
2025-06-23 21:06:37 -0600

    Fixed a weather emoji problem
    The weather symbol was not loading properly with all weathers. Did some tweaking to make it work.

--- config/polybar/scripts/change-config.sh
+++ config/polybar/scripts/change-config.sh
@@ -61,11 +61,13 @@ padding-right = $PADDING_RIGHT
 modules-left = weather
 enable-click = true
 font-0 = "$FONT"
+font-1 = "$FONT_WEATHER"
 
 [module/weather]
 type = custom/script
 exec = curl -s 'wttr.in/?format=%c%t' | sed -E 's/ ([+])?([0-9])/\2/'
 interval = 600
+format = <label>
 label = %output%
 
 [bar/groupsbar]
