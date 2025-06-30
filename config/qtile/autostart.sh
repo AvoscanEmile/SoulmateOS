d90216cc2db3e754c04578af4c28ec45716db9a7
Emile Avoscan <85992291+AvoscanEmile@users.noreply.github.com>
2025-06-30 15:11:45 -0600

    Update autostart.sh
    Added Eww and its volume widget to autostart.sh

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -7,8 +7,11 @@ polybar weather &
 polybar groupsbar &
 polybar volumebar &
 polybar netbar &
+pkill eww
+eww daemon &
+CURRENT_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
+eww update volume="$CURRENT_VOL"
 pkill xfce4-clipman
 xfce4-clipman &
-xset s 1200 1200 
+xset s 1200 1200
 xset dpms 1200 1800 0
-
